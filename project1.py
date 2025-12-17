242050b7f7220eb77fcdf14b469c73546736d8e8
giya007 <giyafrancis007@gmail.com>
2025-12-18 02:29:13 +0530

    Update project1.py

--- project1.py
+++ project1.py
@@ -1,118 +1,100 @@
-import sys
-book_inventory=open("available.txt","w")
-book_inventory.write("111| Atomic Habits | James Clear | 2\n")
-book_inventory.write("211| Fluent Python | Luciano Ramalho | 6\n")
-book_inventory.write("311| Intoduction to Algorithms | Thomas H. Cormen | 4\n")
-book_inventory.write("411| Power of Subconscious Mind | Joseph Murphy | 3\n")
-book_inventory.write("511| Harry Potter and the Sorcerer's Stone | JK Rowling | 2\n")
-book_inventory.close()
+import os
 def view_books():
-    book_inventory = open("available.txt", "w")
-    print("BOOK ID. | BOOK NAME | AUTHOR | NO. OF COPIES AVAILABLE")
-    books_view=book_inventory.read()
-    print(books_viewbook)
-    print("\n--- Issued Books ---")
-    book_inventory.close()
-    try:
-        with open("issued.txt", "r"):
-            book_issued=open("issued.txt","r")
-            issued_book=book_issued.read()
-            print("BOOK ID. | BOOK NAME | AUTHOR | NO. OF COPIES ISSUED")
-            print(issued_book)
-            book_issued.close()      
-    except FileNotFoundError:
-        print("File does not exist")
+        AVAILABLE_BOOKS=open("available.txt",'r')
+        print("----------AVAILABLE BOOKS-------------")
+        print("BOOK ID | NAME | AUTHOR")
+        for i in AVAILABLE_BOOKS:
+            print(i.strip())
+        AVAILABLE_BOOKS.close()
+        print('\n')
+        print("---------ISSUED BOOKS--------------")
+        ISSUED_BOOKS=open("issued.txt",'r')
+        print("BOOK ID | NAME | AUTHOR")
+        for i in ISSUED_BOOKS:
+            print(i.strip())
+        ISSUED_BOOKS.close()
 def add_book():
-    book = input("Enter book name to add: ").strip()
-    copies = int(input("Enter number of copies: ").strip())
-
-    available = read_file_dict(AVAILABLE)
-
-    if book in available:
-        available[book] += copies
-    else:
-        available[book] = copies
-
-    write_file_dict(AVAILABLE, available)
-    print(f"Added {copies} copies of '{book}'.\n")
-
-
+        f2=open("available.txt",'a+')
+        book=[]
+        book.append(int(input("Enter book ID:")))
+        book.append(input("Enter book name:"))
+        book.append(input("Enter author name:"))
+        f2.writelines(str(book)+'\n')
+        print("Book added successfully")
+        f2.close()
 def issue_book():
-    book = input("Enter book name to issue: ").strip()
-    copies = int(input("Enter copies to issue: ").strip())
-
-    available = read_file_dict(AVAILABLE)
-    issued = read_file_dict(ISSUED)
-
-    if book not in available:
-        print("Book not available.\n")
-        return
-
-    if available[book] < copies:
-        print("Not enough copies available.\n")
-        return
-
-    # update numbers
-    available[book] -= copies
-    if available[book] == 0:
-        del available[book]
-
-    issued[book] = issued.get(book, 0) + copies
-
-    write_file_dict(AVAILABLE, available)
-    write_file_dict(ISSUED, issued)
-
-    print(f"Issued {copies} copies of '{book}'.\n")
-
-
+   book_id=input("Enter book ID:")
+   ln=len(book_id)
+   f3=open("available.txt",'r')
+   f4=open("issued.txt",'w')
+   f5=open("temp.txt",'w')
+   check=0
+   for line in f3:
+      if check==0:
+         if line[1:ln+1]==book_id and line[ln+1].isdigit()==False:
+            check=1
+            f4.write(line)
+            continue
+         else:
+            f5.write(line)
+      else:
+         f5.write(line)
+   f3.close()
+   f4.close()
+   f5.close()
+   if check==1:
+      print("Book issued successfully")
+   else:
+      print("No such book")
+   os.remove("available.txt")
+   os.rename("temp.txt","available.txt")
 def return_book():
-    book = input("Enter book name to return: ").strip()
-    copies = int(input("Enter copies to return: ").strip())
-
-    available = read_file_dict(AVAILABLE)
-    issued = read_file_dict(ISSUED)
-
-    if book not in issued or issued[book] < copies:
-        print("Error: too many copies being returned or book never issued.\n")
-        return
-
-    issued[book] -= copies
-    if issued[book] == 0:
-        del issued[book]
-
-    available[book] = available.get(book, 0) + copies
-
-    write_file_dict(AVAILABLE, available)
-    write_file_dict(ISSUED, issued)
-
-    print(f"Returned {copies} copies of '{book}'.\n")
-
-
-def menu():
-    while True:
-        print("==== Library Management System ====")
-        print("1. View Books")
-        print("2. Add Book")
-        print("3. Issue Book")
-        print("4. Return Book")
-        print("5. Exit")
-        choice = input("Enter choice: ")
-        if choice == "1":
-            view_books()
-        elif choice == "2":
-            add_book()
-        elif choice == "3":
-            issue_book()
-        elif choice == "4":
-            return_book()
-        elif choice == "5":
-            print("Goodbye!")
-            break
+    book_id=input("Enter book ID:")
+    ln=len(book_id)
+    f6=open("issued.txt",'r')
+    f7=open("available.txt",'w')
+    f8=open("temp.txt",'w')
+    check=0
+    for line in f6:
+        if check==0:
+            if line[1:ln+1]==book_id and line[ln+1].isdigit()==False:
+                check=1
+                f7.write(line)
+                continue
+            else:
+                f8.write(line)
         else:
-            print("Invalid choice.\n")
-        input("Do you wish to continue?(True/False): ")
-
-menu()
+            f8.write(line)
+    f6.close()
+    f7.close()
+    f8.close()
+    if check==1:
+        print("Book returned successfully")
+    else:
+        print("No such book")
+    os.remove("issued.txt")
+    os.rename("temp.txt","issued.txt")
+ans='y'
+while ans in('y','Y'):
+       print("==== Library Management System ====")
+       print("1.View Books")
+       print("2.Add Book")
+       print("3.Issue Book")
+       print("4.Return Book")
+       print("5.Exit")
+       choice=input("Enter choice:")
+       if choice=="1":
+           view_books()
+       elif choice=="2":
+           add_book()
+       elif choice=="3":
+           issue_book()
+       elif choice=="4":
+           return_book()
+       else:
+           print("Invalid choice")
+       ans=input("Do you wish to continue?(Y/N):")
+    
 
 
 
